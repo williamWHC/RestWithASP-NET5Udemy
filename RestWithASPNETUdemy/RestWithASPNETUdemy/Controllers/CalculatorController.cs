21afc4477353640b0f1c6d9587ef94b234d641b9
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2023-11-03 18:46:21 -0300

    Implementing calculator

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -14,11 +14,39 @@ public class CalculatorController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet("sun/{firstNunber}/{secondNunber}")]
-    public ActionResult Get()
+    [HttpGet("sun/{firstNumber}/{secondNumber}")]
+    public ActionResult Get(string firstNumber, string secondNumber)
     {
-
+        if (IsNumber(firstNumber) && IsNumber(secondNumber))
+        {
+            decimal sun = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(sun.ToString());
+        }
 
         return BadRequest("Invalid Input");
     }
+    private bool IsNumber(string strNumber)
+    {
+        double number;
+
+        bool IsNumber = double.TryParse(
+            strNumber, 
+            System.Globalization.NumberStyles.Any, 
+            System.Globalization.NumberFormatInfo.InvariantInfo, 
+            out number);
+
+        return IsNumber; 
+    }
+
+    private decimal ConvertToDecimal(string srtNumber)
+    {
+        decimal numberValue;
+        if (decimal.TryParse(srtNumber, out numberValue))
+        {
+            return numberValue;
+        }
+        return 0;
+
+    }
+
 }
