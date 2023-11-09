5f34eb13cded2a606e93a26f42249444267ed3ca
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2023-11-09 16:45:29 -0300

    :rocket: Working with four verbs.

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public required string FirtName { get; set; }
+        public required string LastName { get; set; }
+        public required string Address { get; set; }
+        public required string Gender { get; set; }
+    }
+}
