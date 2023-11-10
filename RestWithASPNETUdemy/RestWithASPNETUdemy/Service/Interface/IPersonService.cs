94c5a05da9e086337dac695c10b61e5de7f9f615
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2023-11-10 12:39:04 -0300

    :rocket: implementing the IPersonService interface

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Service/Interface/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Service.Interface
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByiD(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
